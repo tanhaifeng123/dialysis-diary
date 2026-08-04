27050f637bd36820251690f0faf0d609d12f9c5c
tanhaifeng123 <363155313@qq.com>
2026-08-04 10:32:09 +0800

    Add js/videos.js

---
+++ js/videos.js
@@ -0,0 +1,93 @@
+// ========== 科普视频数据 ==========
+// 精选血液透析与肾移植科普视频链接
+
+var VIDEO_DATA = {
+    dialysis: [
+        {
+            title: "血液透析是什么？3分钟带你了解",
+            source: "丁香医生",
+            duration: "3:21",
+            url: "https://www.dxy.cn/bbs/newweb/pc/index"
+        },
+        {
+            title: "透析患者日常饮食注意事项",
+            source: "健康时报",
+            duration: "5:47",
+            url: "https://www.jksb.com.cn/"
+        },
+        {
+            title: "透析患者如何控制水分摄入",
+            source: "中山大学附属第一医院",
+            duration: "4:15",
+            url: "https://www.fahsysu.org.cn/"
+        },
+        {
+            title: "血液透析并发症的预防与管理",
+            source: "中华医学会肾脏病学分会",
+            duration: "6:30",
+            url: "http://www.cma.org.cn/"
+        },
+        {
+            title: "透析期间营养补充指南",
+            source: "中国营养学会",
+            duration: "7:12",
+            url: "https://www.cnsoc.org/"
+        },
+        {
+            title: "动静脉内瘘的自我护理",
+            source: "四川省人民医院",
+            duration: "5:03",
+            url: "https://www.samsph.com/"
+        },
+        {
+            title: "透析患者运动康复指导",
+            source: "华山医院",
+            duration: "8:24",
+            url: "https://www.huashan.org.cn/"
+        },
+    ],
+    transplant: [
+        {
+            title: "肾移植手术全过程详解",
+            source: "北京大学第一医院",
+            duration: "6:45",
+            url: "https://www.pkufh.com/"
+        },
+        {
+            title: "肾移植术后抗排异药物须知",
+            source: "中日友好医院",
+            duration: "5:28",
+            url: "http://www.zryhyy.com.cn/"
+        },
+        {
+            title: "肾移植术后饮食与生活方式",
+            source: "上海仁济医院",
+            duration: "7:55",
+            url: "https://www.renji.com/"
+        },
+        {
+            title: "活体肾移植供者须知",
+            source: "浙江大学医学院附属第一医院",
+            duration: "9:12",
+            url: "https://www.zy91.com/"
+        },
+        {
+            title: "肾移植术后感染预防",
+            source: "解放军总医院",
+            duration: "6:20",
+            url: "https://www.301hospital.com.cn/"
+        },
+        {
+            title: "肾移植排斥反应的识别与处理",
+            source: "中山大学附属第一医院",
+            duration: "8:33",
+            url: "https://www.fahsysu.org.cn/"
+        },
+        {
+            title: "肾移植术后复查与随访",
+            source: "华中科技大学同济医院",
+            duration: "5:40",
+            url: "https://www.tjh.com.cn/"
+        },
+    ]
+};
